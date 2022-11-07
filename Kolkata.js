3ade16dc13355d5e4a20ff4b540e936671846542
saru-79-mathi <113426282+saru-79-mathi@users.noreply.github.com>
2022-11-07 22:16:52 +0530

    Create Kolkata.js

---
+++ Kolkata.js
@@ -0,0 +1,23 @@
+import './kolkata.css';
+import {Link} from 'react-router-dom';
+
+import {Stack,Box} from '@mui/material';
+
+
+const Kolkata=()=>{
+    return(
+       <div className="kolkatabg">
+        <button>Back</button>
+        <Link to="./Home"></Link>
+            <Stack direction="row">
+                <Box flex={1} sx={{height:'500px'}}><button id='btn1'>Cab</button></Box>
+                <Box flex={1} sx={{height:'500px'}}><button id='btn2'>Food</button></Box>
+            </Stack>
+            <Stack direction="row">
+                <Box flex={1} sx={{height:'500px'}}><button id='btn3'>Resort</button></Box>
+                <Box flex={1} sx={{height:'500px'}}><button id='btn4'>Places</button></Box>
+            </Stack>
+       </div>
+    )
+}
+export default Kolkata;
